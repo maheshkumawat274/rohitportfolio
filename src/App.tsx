36e7cb5df81dad9c21c95f6a69ffaf5067fb9368
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-08-06 07:57:12 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Headerpage from "./pages/header";
 import ScrollTop from "./components/ScrollTop";
 import ConsultationPage from "./pages/consultation";
 
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 function App() {
